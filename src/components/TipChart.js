a4fdfe0e8e4ccd34977d2feb6d78b55ec8b413dc
gcampbell391 <gcampbell391@gmail.com>
2020-10-15 11:56:19 -0400

    fixed earnings analytics bugs

--- src/components/TipChart.js
+++ src/components/TipChart.js
@@ -7,35 +7,41 @@ const TipChart = (props) => {
 
     return (
         <div>
-            <Chart
-                className='tip-chart'
-                height={'500px'}
-                chartType="ScatterChart"
-                loader={<div>Loading Chart</div>}
-                data={props.shifts}
-                options={{
-                    pointSize: 15,
-                    pointShape: { type: 'star', sides: 5 },
-                    //Need to figure out how to make chart only animate at start
-                    // animation: {
-                    //     duration: 1000,
-                    //     startup: true //This is the new option
-                    // },
-                    hAxis: {
-                        title: 'Date',
-                    },
-                    vAxis: {
-                        title: 'Total Tip Out In Dollars',
-                        viewWindow: {
-                            min: 0
-                        }
-                    },
-                    series: {
-                        1: { curveType: 'function' },
-                    },
-                }}
-                rootProps={{ 'data-testid': '2' }}
-            />
+            {props.shifts.length > 1 ?
+                <Chart
+                    className='tip-chart'
+                    height={'500px'}
+                    chartType="ScatterChart"
+                    loader={<div>Loading Chart</div>}
+                    data={props.shifts}
+                    options={{
+                        pointSize: 15,
+                        pointShape: { type: 'star', sides: 5 },
+                        //Need to figure out how to make chart only animate at start
+                        // animation: {
+                        //     duration: 1000,
+                        //     startup: true //This is the new option
+                        // },
+                        hAxis: {
+                            title: 'Date',
+                        },
+                        vAxis: {
+                            title: 'Total Tip Out In Dollars',
+                            viewWindow: {
+                                min: 0
+                            }
+                        },
+                        series: {
+                            1: { curveType: 'function' },
+                        },
+                    }}
+                    rootProps={{ 'data-testid': '2' }}
+                />
+                :
+                <div>
+                    <h1>Please Enter A Shift To Generate The Chart</h1>
+                </div>
+            }
         </div>
     )
 }
